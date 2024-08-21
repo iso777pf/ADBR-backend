fe794e00d46b6fcd52d4648fba038f517b7ebb20
Ilja Schorr <ilja.schorr@prime-force.com>
2024-08-21 13:40:14 +0200

    Initial commit with Spring Boot setup

---
+++ src/main/java/com/pf/adbr/AdbrApplication.java
@@ -0,0 +1,13 @@
+package com.pf.adbr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdbrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdbrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pf/adbr/AdbrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pf.adbr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdbrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
